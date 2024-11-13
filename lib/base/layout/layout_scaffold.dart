c34bd831c3ca18b0bec914238772b99799d219da
Nicola Buompane <61230893+NicolaBuomp@users.noreply.github.com>
2024-11-13 14:42:07 +0100

    nuova button navigation bar

---
+++ lib/base/layout/layout_scaffold.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:meteo_app_notification/Base/models/destinations.dart';
+
+class LayoutScaffold extends StatelessWidget {
+  const LayoutScaffold({
+    required this.navigationShell,
+    Key? key,
+  }) : super(key: key ?? const ValueKey<String>('LayoutScaffold'));
+
+  final StatefulNavigationShell navigationShell;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: navigationShell,
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.only(
+            left: 16.0,
+            right: 16.0,
+            bottom: 20.0,
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(24), // Bordi arrotondati
+            child: Container(
+              height: 80,
+              color: Theme.of(context).colorScheme.surface,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: destinations.map((destination) {
+                  final bool isSelected = destinations.indexOf(destination) ==
+                      navigationShell.currentIndex;
+                  return GestureDetector(
+                    onTap: () => navigationShell
+                        .goBranch(destinations.indexOf(destination)),
+                    child: AnimatedContainer(
+                      duration: const Duration(
+                          milliseconds: 300), // Animazione più lunga
+                      curve: Curves.easeInOut, // Curva più fluida
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 8),
+                      decoration: BoxDecoration(
+                        color: isSelected
+                            ? Theme.of(context).primaryColor
+                            : Colors.transparent,
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Row(
+                          children: [
+                            AnimatedSwitcher(
+                              duration: const Duration(milliseconds: 300),
+                              switchInCurve: Curves.easeInOut,
+                              switchOutCurve: Curves.easeInOut,
+                              child: Icon(
+                                destination.icon,
+                                key: ValueKey<bool>(isSelected),
+                                color: isSelected ? Colors.white : Colors.grey,
+                                size: isSelected
+                                    ? 28
+                                    : 24, // Differenza dimensione
+                              ),
+                            ),
+                            AnimatedSize(
+                              duration: const Duration(milliseconds: 300),
+                              curve: Curves.easeInOut,
+                              child: isSelected
+                                  ? Padding(
+                                      padding: const EdgeInsets.only(left: 8.0),
+                                      child: Text(
+                                        destination.label,
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    )
+                                  : const SizedBox.shrink(),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+          ),
+        ),
+      );
+}
--- lib/base/router/router.dart
+++ lib/base/router/router.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:meteo_app_notification/Base/Router/routes.dart';
-import 'package:meteo_app_notification/Base/layout/layou_scaffold.dart';
+import 'package:meteo_app_notification/base/layout/layout_scaffold.dart';
 import 'package:meteo_app_notification/settings/ui/screens/settings/settings_page.dart';
 import 'package:meteo_app_notification/weather/ui/screens/weather_page.dart';
 
